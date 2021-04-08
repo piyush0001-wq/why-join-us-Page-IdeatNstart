bef74126d3c6e40bd034f51a2c72690ed8f34423
piyush0001-wq <piyushsolakhiya@gmail.com>
2021-04-08 12:29:37 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <WhyJoinUS/>
+  </div>
+</template>
+
+<script>
+import WhyJoinUS from './components/WhyJoinUS.vue'
+
+export default {
+  name: 'App',
+  components: {
+    WhyJoinUS
+  }
+}
+</script>
+<style>
+*{
+  margin: 0;
+}
+</style>
+
---
+++ src/components/WhyJoinUS.vue
@@ -0,0 +1,412 @@
+<template>
+  <div class="container">
+   <div class="banner">
+     <h2>Work Flow</h2>
+   </div>
+   <div class="section-one">
+     <div class="icons">
+       <div class="icon"></div>
+       <div><h1>1</h1></div>
+     </div>
+     <div class="content">
+       <h3>Brainstroming</h3>
+       <ul>
+        <li>
+           Having no problem statement - Start with Brainstroming on the plateform
+         </li>
+         <li>
+           Having no idea - Start from the problem statement
+         </li>
+         <li>
+           Participate in the IdeateNstart free communities discussion based on your interest and experience in a specific domain like Healthcare, Agriculture, etc
+         </li>
+         <li>
+           Use IdeateNstart free Brainstroming keywords analysis to get a real world problem statement
+         </li>
+       </ul>
+     </div>
+   </div>
+   <div class="section-two">
+     <div class="icons">
+       <div><h1>2</h1></div>
+       <div class="icon"></div>
+     </div>
+     <div class="content">
+       <h3>Problem Statement</h3>
+       <ul>
+         <li>
+           Having no idea - Start from the Problem statement.
+         </li>
+         <li>
+           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus, vel?
+         </li>
+         <li>
+           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus, vel?
+         </li>
+         <li>
+           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus, vel?
+         </li>
+         <li>
+           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus, vel?
+         </li>
+       </ul>
+     </div>
+   </div>
+   <div class="section-three">
+     <div class="icons">
+       <div class="icon"></div>
+       <div><h1>3</h1></div>
+     </div>
+     <div class="content">
+       <h3>Brainstroming</h3>
+       <ul>
+         <li>
+           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus, vel?
+         </li>
+         <li>
+           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus, vel?
+         </li>
+         <li>
+           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus, vel?
+         </li>
+         <li>
+           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus, vel?
+         </li>
+         <li>
+           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus, vel?
+         </li>
+       </ul>
+     </div>
+   </div>
+   <div class="section-four">
+     <div class="icons">
+       <div><h1>4</h1></div>
+       <div class="icon"></div>
+     </div>
+     <div class="content">
+       <h3>Brainstroming</h3>
+       <ul>
+         <li>
+           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus, vel?
+         </li>
+         <li>
+           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus, vel?
+         </li>
+         <li>
+           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus, vel?
+         </li>
+         <li>
+           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus, vel?
+         </li>
+         <li>
+           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus, vel?
+         </li>
+       </ul>
+     </div>
+   </div>
+   <div class="section-five">
+     <div class="icons">
+       <div class="icon"></div>
+       <div><h1>5</h1></div>
+     </div>
+     <div class="content">
+       <h3>Brainstroming</h3>
+       <ul>
+         <li>
+           Having no problem statement - Start with Brainstroming on the plateform
+         </li>
+         <li>
+           Having no idea - Start from the problem statement
+         </li>
+         <li>
+           Participate in the IdeateNstart free communities discussion based on your interest and experience in a specific domain like Healthcare, Agriculture, etc
+         </li>
+         <li>
+           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus, vel?
+         </li>
+         <li>
+           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus, vel?
+         </li>
+       </ul>
+     </div>
+   </div>
+   <div class="section-six">
+     <div class="icons">
+       <div><h1>6</h1></div>
+       <div class="icon"></div>
+     </div>
+     <div class="content">
+       <h3>Brainstroming</h3>
+       <ul>
+         <li>
+           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus, vel?
+         </li>
+         <li>
+           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus, vel?
+         </li>
+         <li>
+           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus, vel?
+         </li>
+         <li>
+           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus, vel?
+         </li>
+         <li>
+           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus, vel?
+         </li>
+       </ul>
+     </div>
+   </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped >
+
+.content ul li{
+  padding-right: 20px;
+  font-size: 18px;
+}
+
+.banner{
+  height: 30vh;
+  width: 100%;
+  text-align: center;
+}
+.banner h2{
+  padding: 10vh 0 0 0;
+  font-size: 45px;
+}
+.section-one{
+  height: 30vh;
+  width: 100%;
+  background: #42b983;
+  display: flex;
+}
+
+.section-one .icons{
+  width: 40%;
+  height: 30vh;
+  display: flex;
+}
+.section-one .icons .icon{
+  height: 70px;
+  width: 70px;
+  background: white;
+  border-radius: 50%;
+  margin: 50px 10px 0 140px;
+}
+.section-one .icons h1{
+  margin: 30px 0 0 180px;
+  font-size: 45px;
+  color: white;
+}
+.section-one .content{
+  text-align: left;
+  width: 60%;
+  height: 30vh;
+
+}
+.section-one .content h3{
+  font-size: 30px;
+  margin: 10px 25px;
+  color: white;
+}
+.section-one .content li{
+  color: white;
+ 
+}
+/* ///////////////////////////////////////////////////////////////////// */
+.section-two{
+  height: 30vh;
+  width: 100%;
+  background: white;
+  display: flex;
+}
+
+.section-two .icons{
+  width: 40%;
+  height: 30vh;
+  display: flex;
+}
+.section-two .icons .icon{
+  height: 70px;
+  width: 70px;
+  background: #42b983;
+  border-radius: 50%;
+  margin: 50px 10px 0 140px;
+}
+.section-two .icons h1{
+  margin: 30px 0 0 180px;
+  font-size: 45px;
+  color: #42b983;
+}
+.section-two .content{
+  text-align: left;
+   width: 60%;
+  height: 30vh;
+}
+.section-two .content h3{
+  font-size: 30px;
+  margin: 10px 25px;
+  color: #42b983;
+  /* text-shadow: 0 3px 3.5px  #42b983; */
+}
+/* ///////////////////////////////////////////////////////////////////// */
+.section-three{
+  height: 30vh;
+  width: 100%;
+  background: #42b983;
+  display: flex;
+}
+
+.section-three .icons{
+  width: 40%;
+  height: 30vh;
+  display: flex;
+}
+.section-three .icons .icon{
+  height: 70px;
+  width: 70px;
+  background: white;
+  border-radius: 50%;
+  margin: 50px 10px 0 140px;
+}
+.section-three .icons h1{
+  margin: 30px 0 0 180px;
+  font-size: 45px;
+  color: white;
+}
+.section-three .content{
+  text-align: left;
+   width: 60%;
+  height: 30vh;
+}
+.section-three .content h3{
+  font-size: 30px;
+  margin: 10px 25px;
+  color: white;
+}
+.section-three .content li{
+  color: white;
+  
+}
+/* ///////////////////////////////////////////////////////////////////// */
+.section-four{
+  height: 30vh;
+  width: 100%;
+  background: white;
+  display: flex;
+}
+
+.section-four .icons{
+  width: 40%;
+  height: 30vh;
+  display: flex;
+}
+.section-four .icons .icon{
+  height: 70px;
+  width: 70px;
+  background: #42b983;
+  border-radius: 50%;
+  margin: 50px 10px 0 140px;
+}
+.section-four .icons h1{
+  margin: 30px 0 0 180px;
+  font-size: 45px;
+  color: #42b983;
+}
+.section-four .content{
+  text-align: left;
+   width: 60%;
+  height: 30vh;
+}
+.section-four .content h3{
+  font-size: 30px;
+  margin: 10px 25px;
+  color: #42b983;
+}
+
+/* ///////////////////////////////////////////////////////////////////// */
+.section-five{
+  height: 30vh;
+  width: 100%;
+  background: #42b983;
+  display: flex;
+}
+
+.section-five .icons{
+  width: 40%;
+  height: 30vh;
+  display: flex;
+}
+.section-five .icons .icon{
+  height: 70px;
+  width: 70px;
+  background: white;
+  border-radius: 50%;
+  margin: 50px 10px 0 140px;
+}
+.section-five .icons h1{
+  margin: 30px 0 0 180px;
+  font-size: 45px;
+  color: white;
+}
+.section-five .content{
+  text-align: left;
+   width: 60%;
+  height: 30vh;
+}
+.section-five .content h3{
+  font-size: 30px;
+  margin: 10px 25px;
+  color: white;
+}
+.section-five .content li{
+  color: white;
+  
+}
+/* ///////////////////////////////////////////////////////////////////// */
+.section-six{
+  height: 30vh;
+  width: 100%;
+  background: white;
+  display: flex;
+}
+
+.section-six .icons{
+  width: 40%;
+  height: 30vh;
+  display: flex;
+}
+.section-six .icons .icon{
+  height: 70px;
+  width: 70px;
+  background: #42b983;
+  border-radius: 50%;
+  margin: 50px 10px 0 140px;
+}
+.section-six .icons h1{
+  margin: 30px 0 0 180px;
+  font-size: 45px;
+  color: #42b983;
+}
+.section-six .content{
+  text-align: left;
+   width: 60%;
+  height: 30vh;
+}
+.section-six .content h3{
+  font-size: 30px;
+  margin: 10px 25px;
+  color: #42b983;
+}
+/* ///////////////////////////////////////////////////////////////////// */
+</style>
